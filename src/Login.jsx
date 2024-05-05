02649c2c53f40b488e072b3e41ee98808721a43a
dineshs-toast <86337790+dineshs-toast@users.noreply.github.com>
2024-05-05 21:22:47 +0530

    login form details with local validation

---
+++ src/Login.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+export const LoginApp = () => {
+
+    const [username, setUsername] = React.useState('');
+    const [pwd, setPwd] = React.useState('');
+
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+    };
+
+    const handlePwdChange = (e) => {
+        setPwd(e.target.value);
+    };
+
+    const [outputDtls, setOutput] = React.useState('');
+
+    const calcDetails = () => {
+        console.log(username);
+        console.log(pwd)
+        setOutput(username === 'test@gmail.com' && pwd === '123456789' ? 'Login successful' : 'Login Failed. Try again!!!');
+    };
+
+    return <div>
+        <h1>Assignment 2 - Login</h1>
+        <form onSubmit={e => { // <-- form submit callback
+            e.preventDefault(); // prevent default form action so page doesn't reload
+            // console.log(username);
+            // console.log(pwd);
+            calcDetails(username, pwd); // update state
+            setUsername(''); // reset input value
+            setPwd(''); // reset input value
+          }}>
+
+            <label style={{fontWeight:"bold" }}>User Name:</label>
+            <input 
+                id="username" 
+                value={username} 
+                onChange={handleUsernameChange}
+                type="text" 
+                placeholder="Enter user name/email" 
+                style={{marginLeft:"5px"}}>
+            </input>
+            <br />
+            <label style={{fontWeight:"bold" }}>Password:</label>
+            <input 
+                id="pwd" 
+                value={pwd}
+                onChange={handlePwdChange} 
+                type="password" 
+                placeholder="Enter password"
+                style={{marginLeft:"5px"}}>
+            </input>
+            <br />
+            <button type="submit" className="button">Login</button>
+            <br />
+            <br />
+            <label>{outputDtls}</label>
+        </form>
+    </div>
+}
